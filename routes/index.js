ddc2cb47c75f4d1b2aa26eab987858014caa885f
mmswiderski <mmswiderski@gmail.com>
2021-04-01 21:58:04 +0200

    fix: hgighligh express based on search string

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Świat kawy' });
+router.get('/', function (req, res, next) {
+  res.render('index', {title: 'Świat kawy'});
 });
-router.get('/oferty', function(req, res, next) {
-  res.render('oferty', { title: 'Oferty specjalne' });
+router.get('/oferty', function (req, res, next) {
+  res.render('oferty', {title: 'Oferty specjalne'});
 });
-router.get('/ekspresy', function(req, res, next) {
-  res.render('ekspresy', { title: 'Ekspresy', my_var: req.params.ekspres });
+router.get('/ekspresy', function (req, res, next) {
+  res.render('ekspresy', {title: 'Ekspresy', my_var: req.params.ekspres, s:''});
 });
-router.get('/uslugi', function(req, res, next) {
-  res.render('uslugi', { title: 'Nasze usługi' })
+router.post('/ekspresy', (req, res) => {
+  let searchString = ""
+  if (req.body.search) {
+    searchString = req.body.search.toLowerCase();
+  }
+  res.render('ekspresy', {title: 'Ekspresy', my_var: req.params.ekspres, s: searchString});
 });
-router.get('/kontakt', function(req, res, next) {
-  res.render('kontakt', { title: 'Zapraszamy do kontaktu' });
+router.get('/uslugi', function (req, res, next) {
+  res.render('uslugi', {title: 'Nasze usługi'})
+});
+router.get('/kontakt', function (req, res, next) {
+  res.render('kontakt', {title: 'Zapraszamy do kontaktu'});
 });
 
 module.exports = router;
