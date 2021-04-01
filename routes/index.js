5e8d9495124982b06140b4131c7a28455602eda3
Zannneta <zaneta.komos@gmail.com>
2021-04-01 10:54:32 +0200

    Zmiany

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,8 @@ router.get('/', function(req, res, next) {
 router.get('/oferty', function(req, res, next) {
   res.render('oferty', { title: 'Oferty specjalne' });
 });
-router.get('/ekspresy/ekspres/:ekspres', function(req, res, next) {
+router.get('/ekspresy', function(req, res, next) {
   res.render('ekspresy', { title: 'Ekspresy', my_var: req.params.ekspres });
-  next()
-  for (let e of document.getElementsByName(req.params.ekspres)){
-    e.color = 'red';}
 });
 router.get('/uslugi', function(req, res, next) {
   res.render('uslugi', { title: 'Nasze usługi' })
